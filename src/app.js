bf84b841957a99833e16f4549326253d52c4d8af
LeonErasmusCh <leon@santiagobiltong.cl>
2021-09-08 23:10:32 +0000

    done

--- src/app.js
+++ src/app.js
@@ -6,6 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let deck = ["♦", "♥", "♣", "♠"];
+  let card = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "k"];
+
+  let random_deck = deck[Math.floor(Math.random() * deck.length)];
+  let random_number = card[Math.floor(Math.random() * card.length)];
+
+  let result = [random_deck];
+  let result2 = [random_number];
+
+  for (let element of result) {
+    console.log(element);
+    if (element == "♣" || element == "♠") {
+      console.log("black");
+      document.querySelector(".top").style.color = "black";
+      document.querySelector(".bottom").style.color = "black";
+    } else {
+      console.log("red");
+    }
+  }
+
+  document.querySelector(".top").innerHTML = result;
+  document.querySelector(".bottom").innerHTML = result;
+  document.querySelector(".number").innerHTML = result2;
 };
